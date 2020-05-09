404a1715db78e791a946dc1c0e6b58e49e14e333
wangqiwei <wangqiwei@jd.com>
2020-05-09 13:28:45 +0800

    add FunctionTest

---
+++ src/main/java/lambda/function/Person.java
@@ -0,0 +1,17 @@
+package lambda.function;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String echo(String msg) {
+        return msg;
+    }
+}
