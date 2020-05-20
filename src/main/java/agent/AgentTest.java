67820af8edd748596d0956bc0985ffdaedf940a4
wangqiwei <wangqiwei@jd.com>
2020-05-20 13:28:08 +0800

    add JavaAgent Test

---
+++ src/main/java/agent/AgentTest.java
@@ -0,0 +1,11 @@
+package agent;
+
+/**
+ * @author wangqiwei
+ * @date 2020/05/20 1:07 PM
+ */
+public class AgentTest {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
---
+++ src/main/java/agent/ClassLogger.java
@@ -0,0 +1,33 @@
+package agent;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ClassPathUtils;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.ProtectionDomain;
+
+/**
+ * @author wangqiwei
+ * @date 2020/05/20 1:00 PM
+ */
+public class ClassLogger implements ClassFileTransformer {
+    @Override
+    public byte[] transform(ClassLoader loader,
+                            String className,
+                            Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain,
+                            byte[] classfileBuffer) throws IllegalClassFormatException {
+        try {
+            Path path = Paths.get("classes/" + className + ".class");
+            FileUtils.writeByteArrayToFile(path.toFile(), classfileBuffer);
+        } catch (Throwable ignored) { // ignored, don’t do this at home kids
+            ignored.printStackTrace();
+        } finally {
+            return classfileBuffer;
+        }
+    }
+}
