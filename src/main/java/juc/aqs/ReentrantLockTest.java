f3737b153680e6a62b8473ddfd8111534e6e1bc4
kivi <kivi@MacBook-Pro.lan>
2020-06-23 22:05:34 +0800

    update

--- src/main/java/juc/aqs/ReentrantLockTest.java
+++ src/main/java/juc/aqs/ReentrantLockTest.java
@@ -1,5 +1,8 @@
 package juc.aqs;
 
+import org.junit.Test;
+
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -7,8 +10,47 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date 2020/06/16 12:54 PM
  */
 public class ReentrantLockTest {
-    public static void main(String[] args) {
-        ReentrantLock reentrantLock = new ReentrantLock(true);
+    private ReentrantLock reentrantLock = new ReentrantLock(true);
+
+    @Test
+    public void testLock() {
         reentrantLock.lock();
+        Thread thread = new Thread(() -> {
+            reentrantLock.lock();
+        });
+        thread.start();
     }
+
+    @Test
+    public void testCondition() throws InterruptedException {
+        Condition condition1 = reentrantLock.newCondition();
+        Condition condition2 = reentrantLock.newCondition();
+        Thread thread1 = new Thread(() -> {
+            reentrantLock.lock();
+            try {
+                condition1.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                reentrantLock.unlock();
+            }
+
+        });
+
+        Thread thread2 = new Thread(() -> {
+            reentrantLock.lock();
+            try {
+                condition2.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                reentrantLock.unlock();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+
 }
