e8b4217492b46c9c0eb88e8b0b1162304677db3d
kivi <kivi@MacBook-Pro.lan>
2020-07-07 22:51:04 +0800

    add LoadClassTest

---
+++ src/main/java/pers/kivi/javafragment/clazz/LoadClassTest.java
@@ -0,0 +1,12 @@
+package pers.kivi.javafragment.clazz;
+
+/**
+ * @author wangqiwei
+ * @date 2020/07/07 10:46 PM
+ */
+public class LoadClassTest {
+    public static void main(String[] args) throws ClassNotFoundException {
+        // java.lang.ClassLoader.findLoadedClass方法会判断该类是否被（所有类加载器，不只是当前类加载器）加载过
+        Class<?> str = ClassLoader.getSystemClassLoader().loadClass("java.lang.String");
+    }
+}
