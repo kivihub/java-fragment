226211862352fe681d6bedb612ec1abf6660db6a
wangqiwei <wangqiwei@jd.com>
2020-09-07 15:48:29 +0800

    udpate

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -3,7 +3,10 @@
  * @date 2020/08/12 9:14 PM
  */
 public class MyString {
+    static {
+       System.out.println("here is version 2 static block.");
+    }
     public static void echo() {
-        System.out.println("version 1");
+        System.out.println("version 2");
     }
 }
--- src/main/java/pers/kivi/javafragment/loader/HotLoadClassTest.java
+++ src/main/java/pers/kivi/javafragment/loader/HotLoadClassTest.java
@@ -12,35 +12,38 @@ import java.net.URLClassLoader;
  * @date 2020/07/11 10:04 PM
  */
 public class HotLoadClassTest {
-    private File path = new File("/home/kivi/Desktop");
+    private String pathPrefix = "src/main/java/pers/kivi/javafragment/loader/";
+    private File version1Path = new File(pathPrefix + "version1/");
+    private File version2Path = new File(pathPrefix + "version2/");
 
     @Test
     public void testClassForName() throws Throwable {
-        Class aClass = Class.forName("MyString", true, new MyClassLoader(new URL[]{path.toURL()}));
+        System.out.println(version1Path.getAbsoluteFile());
+        Class aClass = Class.forName("MyString", true, new MyClassLoader(new URL[]{version1Path.toURL()}));
         aClass.getDeclaredMethod("echo").invoke(null);
 
         /** 替换MyString实现，可加载最近类 **/
-        aClass = Class.forName("MyString", true, new MyClassLoader(new URL[]{path.toURL()}));
+        aClass = Class.forName("MyString", true, new MyClassLoader(new URL[]{version2Path.toURL()}));
         aClass.getDeclaredMethod("echo").invoke(null);
     }
 
     @Test
     public void testClassLoader() throws Throwable {
-        Class<?> aClass = new MyClassLoader(new URL[]{path.toURL()}).loadClass("MyString");
+        Class<?> aClass = new MyClassLoader(new URL[]{version1Path.toURL()}).loadClass("MyString");
         aClass.getDeclaredMethod("echo").invoke(null);
 
         /** 替换MyString实现，可加载最近类 **/
-        aClass = new MyClassLoader(new URL[]{path.toURL()}).loadClass("MyString");
+        aClass = new MyClassLoader(new URL[]{version2Path.toURL()}).loadClass("MyString");
         aClass.getDeclaredMethod("echo").invoke(null);
     }
 
     @Test
     public void testEquals() throws ClassNotFoundException, MalformedURLException {
-        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{path.toURL()});
+        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{version1Path.toURL()});
         Class<?> myString = Class.forName("MyString", true, myClassLoader);
         Class<?> myString1 = myClassLoader.loadClass("MyString");
 
-        myClassLoader = new MyClassLoader(new URL[]{path.toURL()});
+        myClassLoader = new MyClassLoader(new URL[]{version2Path.toURL()});
         Class<?> myString2 = myClassLoader.loadClass("MyString");
         Class<?> myString3 = Class.forName("MyString", true, myClassLoader);
 
