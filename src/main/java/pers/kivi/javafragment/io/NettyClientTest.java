717ea27b0616bd86c0ca1ee3bb14183c299d4751
kivi <kivi@MacBook-Pro.lan>
2021-01-30 22:21:08 +0800

    refactor

--- src/main/java/pers/kivi/javafragment/io/NettyClientTest.java
+++ src/main/java/pers/kivi/javafragment/io/NettyClientTest.java
@@ -3,7 +3,6 @@ package pers.kivi.javafragment.io;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.buffer.UnpooledDirectByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -11,7 +10,6 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import org.junit.Test;
 
 import java.util.Scanner;
 
@@ -20,6 +18,10 @@ import java.util.Scanner;
  * @date 2021/01/28 9:59 PM
  */
 public class NettyClientTest {
+    private static final int LISTEN_PORT = 9000;
+    private static final ByteBuf DELIMITER = Unpooled.copiedBuffer("_".getBytes());
+    private static final int MAX_FRAME_LENGTH = 1024;
+
     public static void main(String[] args) throws InterruptedException {
         EventLoopGroup eventExecutors = new NioEventLoopGroup();
         Bootstrap bootstrap = new Bootstrap();
@@ -27,52 +29,56 @@ public class NettyClientTest {
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer("_".getBytes())));
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(MAX_FRAME_LENGTH, DELIMITER));
                         ch.pipeline().addLast(new StringEncoder());
+                        ch.pipeline().addLast(new StringDecoder());
                         ch.pipeline().addLast(new ChatClientHandler());
                     }
                 });
-        ChannelFuture localhost = bootstrap.connect("localhost", 9000);
-        localhost.await();
-        System.out.println("client connect.");
-//        Scanner scanner = new Scanner(System.in);
-//        while (scanner.hasNextLine()) {
-//            String s = scanner.nextLine();
-//            System.out.println("Console input:" + s);
-//            ByteBuf byteBuf = Unpooled.copiedBuffer(s.getBytes());
-//            localhost.channel().writeAndFlush(byteBuf);
-//        }
-//
-        int i = 20;
-        while (i-- >0) {
-            ByteBuf byteBuf = Unpooled.copiedBuffer("kivi_".getBytes());
-            localhost.channel().writeAndFlush(byteBuf);
-        }
 
-        localhost.channel().closeFuture().await();
+        ChannelFuture channelFuture = bootstrap.connect("localhost", LISTEN_PORT);
+        channelFuture.await();
+        System.out.println("Client connected server.");
+
+        sendMsgWithConsole(channelFuture.channel());
+//        sendBatchMsg(channelFuture.channel());
+
+        channelFuture.channel().closeFuture().await();
+    }
+
+    private static void sendBatchMsg(Channel channel) {
+        int i = 20;
+        while (i-- > 0) {
+            ByteBuf byteBuf = Unpooled.copiedBuffer("I am here._".getBytes());
+            channel.writeAndFlush(byteBuf);
+        }
+    }
+
+    private static void sendMsgWithConsole(Channel channel) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            System.out.println("Console input:" + s);
+            channel.writeAndFlush(s + "_");
+        }
     }
 
     static class ChatClientHandler extends ChannelInboundHandlerAdapter {
 
         @Override
-        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-            System.out.println("Client Channel Active");
+        public void channelActive(ChannelHandlerContext ctx) {
+            System.out.println("Client online");
         }
 
         @Override
-        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            System.out.println("Client Channel InActive");
+        public void channelInactive(ChannelHandlerContext ctx) {
+            System.out.println("Client offline");
         }
 
         @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            System.out.println("Client accept: " + msg);
-        }
-
-        @Override
-        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-            System.out.println("Client读取完毕");
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            System.out.println(msg);
         }
     }
 }
--- src/main/java/pers/kivi/javafragment/io/NettyServerTest.java
+++ src/main/java/pers/kivi/javafragment/io/NettyServerTest.java
@@ -1,20 +1,27 @@
 package pers.kivi.javafragment.io;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
  * @author wangqiwei
  * @date 2021/01/25 9:38 PM
  */
 public class NettyServerTest {
+    private static final int LISTEN_PORT = 9000;
+    private static final ByteBuf DELIMITER = Unpooled.copiedBuffer("_".getBytes());
+    private static final int MAX_FRAME_LENGTH = 1024;
 
     public static void main(String[] args) throws InterruptedException {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
@@ -23,43 +30,50 @@ public class NettyServerTest {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
-//                .handler(new ChatServerHandler())
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    public void initChannel(SocketChannel ch)  {
-                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer("_".getBytes())));
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(MAX_FRAME_LENGTH, DELIMITER));
                         ch.pipeline().addLast(new StringDecoder());
                         ch.pipeline().addLast(new StringEncoder());
                         ch.pipeline().addLast(new ChatServerHandler());
                     }
                 });
-        ChannelFuture bind = serverBootstrap.bind(9000);
-        ChannelFuture sync = bind.sync();
-        System.out.println("server started.");
-        sync.channel().closeFuture().await();
+        ChannelFuture channelFuture = serverBootstrap.bind(LISTEN_PORT).sync();
+        System.out.println("ChatServer started.");
+        channelFuture.channel().closeFuture().await();
     }
 
     static class ChatServerHandler extends SimpleChannelInboundHandler {
+        private static ChannelGroup onlineClients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
         @Override
-        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        public void channelActive(ChannelHandlerContext ctx) {
             Channel channel = ctx.channel();
-            System.out.println("new Connection:" + channel.remoteAddress());
+            String msg = String.format("Client[%s] online", channel.remoteAddress());
+
+            onlineClients.add(channel);
+            System.out.println(msg);
+            onlineClients.writeAndFlush(msg + "_", itemChannel -> !itemChannel.equals(channel));
         }
 
         @Override
-        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            super.channelInactive(ctx);
+        public void channelInactive(ChannelHandlerContext ctx) {
+            Channel channel = ctx.channel();
+            String msg = String.format("Client[%s] offline", channel.remoteAddress());
+
+            onlineClients.remove(channel);
+            System.out.println(msg + "_");
+            onlineClients.writeAndFlush(msg + "_", itemChannel -> !itemChannel.equals(channel));
         }
 
         @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-            System.out.println("Server accept: " + msg);
-        }
+        protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+            Channel channel = ctx.channel();
+            msg = String.format("Client[%s] says: %s", channel.remoteAddress(), msg);
 
-        @Override
-        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-//            System.out.println("Server读取完毕");
+            System.out.println(msg);
+            onlineClients.writeAndFlush(msg + "_", itemChannel -> !itemChannel.equals(channel));
         }
     }
 }
