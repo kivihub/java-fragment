6c38f6e4e8e855ea33e7b69391776ef2e2eb328b
王奇伟 <wangqiwei@wangqiweideMBP.lan>
2021-07-04 13:35:37 +0800

    up

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -1,12 +1,48 @@
+import java.math.BigDecimal;
+
 /**
  * @author wangqiwei
  * @date 2020/08/12 9:14 PM
  */
 public class MyString {
     static {
-       System.out.println("here is version 2 static block.");
+        System.out.println("here is version 2 static block.");
     }
-    public static void echo() {
-        System.out.println("version 2");
+
+    public static void main(String[] args) {
+//        System.out.println(new BigDecimal(0.1).setScale(2, BigDecimal.ROUND_UNNECESSARY).longValue());
+        System.out.println(new BigDecimal("1.9").subtract(new BigDecimal("1")).setScale(0, BigDecimal.ROUND_HALF_UP).longValue());
+    }
+
+    public static void main1(String[] args) {
+        double d = 0.2d;
+        System.out.println(d + 1);
+        System.out.println((d + 1) * 100);
+        BigDecimal bigDecimal = new BigDecimal(String.valueOf(d + 1));
+        BigDecimal multiply = bigDecimal.multiply(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_UP);
+        System.out.println(multiply.intValue());
+
+        double d1 = 0.2d;
+        System.out.println(d1 + 1);
+        System.out.println((d1 + 1) * 100);
+        BigDecimal bigDecimal1 = new BigDecimal(String.valueOf(d1 + 1)).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal multiply1 = bigDecimal1.multiply(new BigDecimal(100));
+        System.out.println(multiply1.intValue());
+
+        double d2 = 1.01d;
+        System.out.println(d2 - 1);
+        System.out.println((d2 - 1) * 100);
+        BigDecimal bigDecimal2 = new BigDecimal(String.valueOf(d2 - 1)).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal multiply2 = bigDecimal2.multiply(new BigDecimal(100));
+        System.out.println(multiply2.toBigInteger().intValue());
+//        System.out.println(multiply2.floatValue());
+
+        double d3 = 1.1d;
+        System.out.println(d3 - 1);
+        System.out.println((d3 - 1) * 100);
+        BigDecimal bigDecimal3 = new BigDecimal(String.valueOf(d3 - 1)).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal multiply3 = bigDecimal3.multiply(new BigDecimal(100));
+        System.out.println(multiply3.toBigInteger().intValue());
+//        System.out.println(multiply3.floatValue());
     }
 }
--- src/main/java/pers/kivi/javafragment/httpclient/HttpClientTest.java
+++ src/main/java/pers/kivi/javafragment/httpclient/HttpClientTest.java
@@ -2,6 +2,8 @@ package pers.kivi.javafragment.httpclient;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.config.ConnectionConfig;
@@ -11,6 +13,8 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -26,12 +30,23 @@ import java.util.concurrent.Executors;
  * @date 2021/07/03 23:00
  */
 public class HttpClientTest {
+    Logger logger = LoggerFactory.getLogger(HttpClientTest.class);
+
     @Before
-    public void startHttpServer() throws IOException, InterruptedException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8081), 0);
+    public void startHttpServerOnPort8081() throws IOException, InterruptedException {
+        startHttpServer(8081, "/hello");
+    }
+
+    @Before
+    public void startHttpServerOnPort8082() throws IOException, InterruptedException {
+        startHttpServer(8082, "/world");
+    }
+
+    private void startHttpServer(int port, String path) throws IOException, InterruptedException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
         httpServer.setExecutor(Executors.newFixedThreadPool(10));
         HttpHandler httpHandler = exchange -> {
-            System.out.println("接收请求, Time: " + new Date());
+            logger.info("Server开始处理请求");
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {//ignore
@@ -44,33 +59,48 @@ public class HttpClientTest {
             exchange.close();
         };
 
-        httpServer.createContext("/hello", httpHandler);
-        httpServer.createContext("/world", httpHandler);
+        httpServer.createContext(path, httpHandler);
         httpServer.start();
-        System.out.println("Http Server Listening port 8081");
+        logger.info("Http Server listening on port " + port);
     }
+
 
     @Test
     public void testHttpClient() throws Exception {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
-//                .setMaxConnPerRoute(2).setMaxConnTotal(3)
+                // 每个路由的最大的连接数
+                .setMaxConnPerRoute(1)
+                // 连接池的最大连接数
+                .setMaxConnTotal(20)
                 .build();
         List<String> urls = Arrays.asList("http://localhost:8081/hello", "http://localhost:8081/world");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             int j = i;
             new Thread(() -> {
-                String url = urls.get(j % 2);
-                System.out.println("开始执行：" + j + "; URL: " + url);
-                HttpGet get = new HttpGet(url);
-                try (CloseableHttpResponse result = httpClient.execute(get)) {
-                    String s = EntityUtils.toString(result.getEntity());
-                    System.out.println(s);
-                } catch (Exception e) {  //ignore
-                }
-                System.out.println("结束执行：" + j);
+                doGet(httpClient, urls.get(j % 2));
             }).start();
         }
 
         new CountDownLatch(1).await();
+    }
+
+    private void doGet(CloseableHttpClient httpClient, String url) {
+        logger.info("客户端开始请求URL: " + url);
+        HttpGet get = new HttpGet(url);
+        RequestConfig config = RequestConfig.custom()
+                // 从HttpClient连接池等待连接超时
+                .setConnectionRequestTimeout(10000)
+                // TCP建立连接超时时间
+                .setConnectTimeout(5000)
+                // TCP开始传输数据时，接收到两个packet的最大间隔时间
+                .setSocketTimeout(8000)
+                .build();
+        get.setConfig(config);
+        try (CloseableHttpResponse result = httpClient.execute(get)) {
+            String s = EntityUtils.toString(result.getEntity());
+            logger.info("Http Response: " + s);
+        } catch (Exception e) {
+            logger.error("请求失败", e);
+        }
     }
 }
